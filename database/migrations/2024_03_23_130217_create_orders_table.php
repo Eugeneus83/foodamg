cf11c0df73ab804265b99d11d83cc9cef0b659e5
Eugene <parserok24@gmail.com>
2024-03-23 15:30:30 +0200

    Migrations

---
+++ database/migrations/2024_03_23_130217_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('phone', 50);
+            $table->string('name', 100);
+            $table->string('address', 200);
+            $table->decimal('total', 18);
+            $table->integer('user_id');
+            $table->string('status');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
